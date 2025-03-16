1729ff67a0b5c5dbbdce293fddd40e0b19a6aac6
Iryna-kru <iryna101076@yahoo.com>
2025-03-16 00:45:21 -0400

    OOP

---
+++ utils/test.js
@@ -0,0 +1,5 @@
+// 2 ways 
+// CommonJS (module.exports require) - used with NodeJS
+// ES6 Modules (import export) - are not directly supported with NodeJS but browsers
+// Cypress Playwright -> They use both (both are supported)
+// 
